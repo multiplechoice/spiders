7c42fb265fa596ecfaaf9f59df6c605b9bf4404c
Alexander O'Donovan-Jones <alexodonovanjones@gmail.com>
2017-04-28 14:44:07 +0000

    Add loading of each job page for mbl.is/atvinna.
    This allows us to populate the `posted` field as well.

--- jobs/jobs/spiders/mbl.py
+++ jobs/jobs/spiders/mbl.py
@@ -19,6 +19,16 @@ class MblSpider(scrapy.Spider):
             item = JobsItem()
             item['title'] = job.css('.title::text').extract_first(),
             item['company'] = job.css('div.company-wrapper span.company::text').extract_first(),
-            item['url'] = response.urljoin(job.css('a::attr(href)').extract_first()),
             item['deadline'] = decode_date_string(job.css('span.date::text').extract_first()),
-            yield item
+
+            url = response.urljoin(job.css('a::attr(href)').extract_first())
+            item['url'] = url,
+
+            request = scrapy.Request(url, callback=self.parse_specific_job)
+            request.meta['item'] = item
+            yield request
+
+    def parse_specific_job(self, response):
+        item = response.meta['item']
+        item['posted'] = decode_date_string(response.css('.ad_created::text').re(r'Sett inn: (.+)')[0])
+        yield item
