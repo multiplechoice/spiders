359b9e5c171b6526baa258e4a643852b41c613b0
Alexander O'Donovan-Jones <alexodonovanjones@gmail.com>
2017-06-21 11:45:11 +0000

    Add the PostgresPipeline to output to the RDS instance

--- README.rst
+++ README.rst
@@ -9,6 +9,8 @@ Scrapes `Tvinna <http://www.tvinna.is/>`__, `Morgunblaðið <http://www.mbl.is/a
 
 Updates to spiders are automatically detected by the project on `Scrapinghub <https://scrapinghub.com/>`__ and deployed.
 
+This project relies upon the `multiplechoice/sqlalchemy-mappings` project to maintain the correct database structures.
+
 .. |Build Status| image:: https://travis-ci.org/multiplechoice/spiders.svg?branch=master
   :target: https://travis-ci.org/multiplechoice/spiders
 .. |Coveralls Status| image:: https://coveralls.io/repos/github/multiplechoice/spiders/badge.svg?branch=master
--- jobs/pipelines.py
+++ jobs/pipelines.py
@@ -1,11 +1,41 @@
 # -*- coding: utf-8 -*-
-
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+from mappings import ScrapedJob
+from mappings.utils import session_scope
 
 
-class JobsPipeline(object):
+class PostgresPipeline(object):
+    def __init__(self, settings, stats):
+        self.credentials = settings.get('PG_CREDS')
+        self.scope = session_scope(self.credentials)
+        self.stats = stats
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        return cls(settings=crawler.settings, stats=crawler.stats)
+
+    def open_spider(self, spider):
+        self.session = self.scope.__enter__()
+
+    def close_spider(self, spider):
+        self.scope.__exit__(None, None, None)
+
     def process_item(self, item, spider):
+        job = ScrapedJob.from_dict(dict(item))
+
+        query = self.session.query(ScrapedJob).filter(ScrapedJob.url == job.url)
+        matched_job = query.one_or_none()
+        if matched_job is None:
+            # it's a new job, since it hasn't been seen before
+            self.session.add(job)
+            self.stats.inc_value('postgresql/add')
+        else:
+            if job.posted < matched_job.posted:
+                # new record has an older timestamp
+                matched_job.data['posted'] = job.posted
+                # modifying the existing record will cause it to be marked as dirty
+                # so when the session is committed it will emit an UPDATE for the row
+                self.stats.inc_value('postgresql/modify')
+            else:
+                self.stats.inc_value('postgresql/ignore')
+
         return item
