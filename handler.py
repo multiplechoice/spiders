d74e7e085ba62b3e21f8e3a70813066d323d3aae
Alexander O'Donovan-Jones <alexodonovanjones@gmail.com>
2019-04-09 13:04:17 +0000

    Cleanup handler

--- handler.py
+++ handler.py
@@ -24,34 +24,6 @@ def format_response(response):
 
     return fixed
 
-# stats example:
-# {'downloader/request_bytes': 10055,
-#  'downloader/request_count': 34,
-#  'downloader/request_method_count/GET': 34,
-#  'downloader/response_bytes': 174399,
-#  'downloader/response_count': 34,
-#  'downloader/response_status_count/200': 32,
-#  'downloader/response_status_count/301': 2,
-#  'finish_reason': 'finished',
-#  'finish_time': datetime.datetime(2019, 4, 8, 11, 49, 32, 717749),
-#  'item_scraped_count': 30,
-#  'log_count/DEBUG': 112,
-#  'log_count/INFO': 9,
-#  'log_count/WARNING': 1,
-#  'memusage/max': 76472320,
-#  'memusage/startup': 76472320,
-#  'postgresql/add': 30,
-#  'request_depth_max': 1,
-#  'response_received_count': 32,
-#  'robotstxt/request_count': 1,
-#  'robotstxt/response_count': 1,
-#  'robotstxt/response_status_count/200': 1,
-#  'scheduler/dequeued': 32,
-#  'scheduler/dequeued/memory': 32,
-#  'scheduler/enqueued': 32,
-#  'scheduler/enqueued/memory': 32,
-#  'start_time': datetime.datetime(2019, 4, 8, 11, 49, 22, 841506)}
-
 
 def put_metrics(stats, function_name):
     import boto3
