8db801a3f5524b30ef850d5112f35352b92965b8
Alexander O'Donovan-Jones <alexodonovanjones@gmail.com>
2019-04-13 12:48:02 +0000

    Add proper db test

--- tests/test_pipelines.py
+++ tests/test_pipelines.py
@@ -1,5 +1,5 @@
 from mappings import Base, ScrapedJob
-from mappings.utils import create_engine, install_pgcrypto, create_table, create_user, alter_table_owner
+from mappings.utils import create_engine, install_pgcrypto, create_table, create_user, alter_table_owner, session_scope
 from jobs.pipelines import PostgresPipeline
 from jobs.items import JobsItem
 
@@ -10,7 +10,7 @@ import sqlalchemy  # comes from the sqlalchemy-mappings dependencies
 import mock
 
 
-# use the setup/teardown logic from mappings/test_sessions.py
+# use the setup/teardown logic from sqlalchemy-mappings/test_sessions.py
 SUPERUSER = 'postgresql://postgres@localhost:5432/jobdb'
 DIRK = 'postgresql://dirk_gently@localhost:5432/jobdb'
 
@@ -36,7 +36,7 @@ def db():
     engine.execute("""DROP ROLE IF EXISTS dirk_gently;""")
 
 
-def test_environment_variable():
+def test_postgres_environment_variable():
     # ensure we get the expected exception when not specifying credentials
     with pytest.raises(NotConfigured):
         PostgresPipeline.from_crawler(get_crawler())
@@ -46,7 +46,7 @@ def test_environment_variable():
     assert isinstance(PostgresPipeline.from_crawler(crawler), PostgresPipeline)
 
 
-def test_initialisation():
+def test_postgres_initialisation():
     crawler = get_crawler(settings_dict={'PG_CREDS': DIRK})
     pipeline = PostgresPipeline.from_crawler(crawler)
     assert pipeline.stats.get_value('postgresql/add') is None
@@ -54,7 +54,7 @@ def test_initialisation():
     assert pipeline.stats.get_value('postgresql/ignore') is None
 
 
-def test_statistics():
+def test_postgres_statistics():
     crawler = get_crawler(settings_dict={'PG_CREDS': DIRK})
     pipeline = PostgresPipeline.from_crawler(crawler)
     job = JobsItem(spider='test', url='http://fake/job/advert.html', company='Project Blackwing')
@@ -86,3 +86,32 @@ def test_statistics():
     assert pipeline.stats.get_value('postgresql/add') == 1
     assert pipeline.stats.get_value('postgresql/modify') == 1
     assert pipeline.stats.get_value('postgresql/ignore') == 1
+
+
+def test_postgres_insert(db):
+    crawler = get_crawler(settings_dict={'PG_CREDS': DIRK})
+    pipeline = PostgresPipeline.from_crawler(crawler)
+    test_subject = JobsItem(spider='a', url='http://subject.html', company='Project Blackwing', title='Test Subject')
+    director = JobsItem(spider='a', url='http://director.html', company='Project Blackwing', title='Director')
+
+    # we don't need to provide the `spider` since it's not used, but is required for the inherited
+    # function signature
+    spider = None
+    pipeline.open_spider(spider)
+    # as above, the `spider` argument isn't used, but required for the inherited signature
+    pipeline.process_item(test_subject, spider)
+    pipeline.process_item(director, spider)
+    pipeline.close_spider(spider)
+
+    # similar to tests in sqlalchmey-mappings/test_sessions.py
+    with session_scope(DIRK) as db_session:
+        query = db_session.query(ScrapedJob).filter(ScrapedJob.url == 'http://subject.html')
+        result = query.one()
+        assert result.url == test_subject['url']
+        assert result.data == dict(test_subject)
+
+        query = db_session.query(ScrapedJob).filter(ScrapedJob.url == 'http://director.html')
+        result = query.one()
+        assert result.url == director['url']
+        assert result.spider == director['spider']
+        assert result.data == dict(director)
